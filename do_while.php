a57a53a977240bf3a4c5d25cfa3120ec561608a0
Felice Malaszowski <felice.vargas@gmail.com>
2014-11-21 13:56:57 -0600

    Added Five Loop

--- do_while.php
+++ do_while.php
@@ -1,8 +1,16 @@
 <?php
+// starts at 0, counts by 2, stops at 100
+// $evenNum = 0;
 
-$evenNum = 0;
+// do {
+// 	echo "$evenNum\n";
+// 	$evenNum+=2;
+// } while ($evenNum <= 100);
+
+// starts at 100, counts backwards by 5, stops at -10
+$evenNum = 100;
 
 do {
 	echo "$evenNum\n";
-	$evenNum+=2;
-} while ($evenNum <= 100);
+	$evenNum -= 5;
+} while ($evenNum >= -10);
