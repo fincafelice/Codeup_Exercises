dca2892ee01cfa1c29eefa7c901d7e381353fbf8
Felice Malaszowski <felice.vargas@gmail.com>
2014-11-21 14:09:42 -0600

    Added Multiplication Loop

--- do_while.php
+++ do_while.php
@@ -8,9 +8,19 @@
 // } while ($evenNum <= 100);
 
 // starts at 100, counts backwards by 5, stops at -10
-$evenNum = 100;
+//$evenNum = 100;
+
+// do {
+// 	echo "$evenNum\n";
+// 	$evenNum -= 5;
+// } while ($evenNum >= -10);
+
+// starts at 2, double each time, stop at 1million
+
+$a = 2;
 
 do {
-	echo "$evenNum\n";
-	$evenNum -= 5;
-} while ($evenNum >= -10);
+	echo "$a\n";
+	$a*=$a;
+} while ($a <= 1000000);
+
