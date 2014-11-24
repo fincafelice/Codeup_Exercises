3d6850ec4b6327046a430fc35091ea9ae479f755
Felice Malaszowski <felice.vargas@gmail.com>
2014-11-24 16:08:36 -0600

    This allows user to choose increment

--- for.php
+++ for.php
@@ -3,12 +3,16 @@
 // Prompt user for a starting number and ending number, 
 // then display all the numbers from the starting to ending 
 // using a for loop.
+// Edit where user chooses increment.
 
 fwrite(STDOUT, 'Enter a beginning number:  ');
 $userStart = trim(fgets(STDIN));
 fwrite(STDOUT, 'Enter an ending number:  ');
 $userEnd = trim(fgets(STDIN));
 
-for ($start= $userStart; $start <= $userEnd; $start++) {
+fwrite(STDOUT, 'Enter the increment value:   ');
+$userIncrm = trim(fgets(STDIN));
+
+for ($start= $userStart; $start <= $userEnd; $start+=$userIncrm) {
 	echo "$start\n";
 }
