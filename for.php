e333e7ea1c2e59844efc1d30f09427306cff50d9
Felice Malaszowski <felice.vargas@gmail.com>
2014-11-24 16:21:21 -0600

    added code to default if no increment selected

--- for.php
+++ for.php
@@ -3,15 +3,21 @@
 // Prompt user for a starting number and ending number, 
 // then display all the numbers from the starting to ending 
 // using a for loop.
-// Edit where user chooses increment.
+
 
 fwrite(STDOUT, 'Enter a beginning number:  ');
 $userStart = trim(fgets(STDIN));
 fwrite(STDOUT, 'Enter an ending number:  ');
 $userEnd = trim(fgets(STDIN));
 
+// Edit where user chooses increment.
 fwrite(STDOUT, 'Enter the increment value:   ');
 $userIncrm = trim(fgets(STDIN));
+
+// Default increment to 1 if no input.
+if (!$userIncrm){
+	$userIncrm =1;
+}
 
 for ($start= $userStart; $start <= $userEnd; $start+=$userIncrm) {
 	echo "$start\n";
