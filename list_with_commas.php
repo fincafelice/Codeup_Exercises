a332e4f999680b7fd64a7e7b03d233dd8844a9ad
Felice Malaszowski <felice.vargas@gmail.com>
2014-12-03 15:07:34 -0600

    Default alphabetical sorting to false

--- list_with_commas.php
+++ list_with_commas.php
@@ -1,25 +1,33 @@
 <?php
 
 $physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
+// Create function to allow sorting option.
 
-function humanizedList ($physicistsString) {
+// Change string to array to manipulate data.
+function humanizedList ($physicistsString, $alpha = false) {
+
 
 	$explodePhysicistsString = explode (", ", $physicistsString);
 	// print_r($explodePhysicistsString);
 
-	array_pop ($explodePhysicistsString);
+	if ($alpha) {
+		sort ($explodePhysicistsString);
+	}
+	
+	$lastName = array_pop ($explodePhysicistsString);
 	// print_r($explodePhysicistsString);
 
-	$endingName = 'and Tony Stark';
-	array_push($explodePhysicistsString, $endingName);
-	sort($explodePhysicistsString);
+	// $endingName = 'and Tony Stark';
+	// array_push($explodePhysicistsString, $endingName);
+	// sort($explodePhysicistsString);
 	// print_r($explodePhysicistsString);
 
 	$famousFakePhysicists = implode (", ", $explodePhysicistsString);
-	return "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}.\n";
+	// return "{$famousFakePhysicists}, and {$lastName}";
+	return "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}, and {$lastName}.\n";
 }
 
-echo humanizedList($physicistsString);
+echo humanizedList($physicistsString, true);
 // print_r($famousFakePhysicists);
 // echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}.\n";
 
