34c966856ee9451707ea75fe87951619d61ddc80
Felice Malaszowski <felice.vargas@gmail.com>
2014-12-03 11:14:39 -0600

    Refactored code with a function

--- list_with_commas.php
+++ list_with_commas.php
@@ -2,17 +2,24 @@
 
 $physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
 
-$explodePhysicistsString = explode (", ", $physicistsString);
-// print_r($explodePhysicistsString);
+function humanizedList ($physicistsString) {
 
-array_pop ($explodePhysicistsString);
-// print_r($explodePhysicistsString);
+	$explodePhysicistsString = explode (", ", $physicistsString);
+	// print_r($explodePhysicistsString);
 
-$endingName = 'and Tony Stark';
-array_push($explodePhysicistsString, $endingName);
-// print_r($explodePhysicistsString);
+	array_pop ($explodePhysicistsString);
+	// print_r($explodePhysicistsString);
 
+	$endingName = 'and Tony Stark';
+	array_push($explodePhysicistsString, $endingName);
+	// print_r($explodePhysicistsString);
 
-$famousFakePhysicists = implode (", ", $explodePhysicistsString);
+	$famousFakePhysicists = implode (", ", $explodePhysicistsString);
+	return "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}.\n";
+}
+
+echo humanizedList($physicistsString);
 // print_r($famousFakePhysicists);
-echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}.\n";
+// echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}.\n";
+
+// print_r($explodePhysicistsString);
